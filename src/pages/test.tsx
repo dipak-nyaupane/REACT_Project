3c0ff6d0fc2dce5cea9dcc1ec2c1b7fb0f7b9565
Dipak Nyaupane <nyaupanedipak@gmail.com>
2023-03-01 14:10:06 +0545

    first commit

---
+++ src/pages/test.tsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/Home.module.css'
+
+export default function test(){
+
+  return (
+    <>
+    <div>
+       <main className={styles.main}>
+
+    <h1>My name is dipak</h1>
+
+</main>
+    </div>
+    </>
+  )
+}
